20b7f8c578c1f93f4bdc9f78bb7bc21e2be3aa0b
git push -u origin <mazairaj94@gmail.com>
2017-03-03 17:11:44 -0800

    heroku

--- app.js
+++ app.js
@@ -174,24 +174,25 @@ app.post('/setupboard',function(req,res) {
   res.json('true');
 })
 app.post('/makemove', function(req, res) {
-  console.log(board)
-  var moves = req.body.moves;
-  var pos2 = moves[1];
-  var pos1 = moves[1];
-  var direction = checkValidMove(pos1,pos2);
-  if(direction==='illegal move') {
-    res.json({
-      board: transpose(board),
-      currentPlayer: currentPlayer,
-      move: [pos1]
-    });
-  };
-  makeMove(position,direction);
-  res.json({
-    board: transpose(board),
-    currentPlayer: nextPlayer(),
-    move: []
-  });
+  console.log(req.body)
+  // var moves = req.body.moves;
+  // var pos2 = moves[1];
+  // var pos1 = moves[1];
+  // var direction = checkValidMove(pos1,pos2);
+  // if(direction==='illegal move') {
+  //   res.json({
+  //     board: transpose(board),
+  //     currentPlayer: currentPlayer,
+  //     move: [pos1]
+  //   });
+  // };
+  // makeMove(position,direction);
+  // res.json({
+  //   board: transpose(board),
+  //   currentPlayer: nextPlayer(),
+  //   move: []
+  // });
+  res.send('true')
 })
 
 app.listen(process.env.PORT||3000)
