c511743ca17eb1cc66d12c9b5833f2d6d30b2e6e
git push -u origin <mazairaj94@gmail.com>
2017-03-05 14:08:28 -0800

    k

--- app.js
+++ app.js
@@ -247,11 +247,12 @@ app.post('/setupboard',function(req,res) {
     board[6] = boardTranspose[6];
     board[7] = boardTranspose[7];
   }
-  
+
   // console.log('Post setupboard ********', board)
   var boardTranspose = (transpose(board, 8));
   res.json({board: boardTranspose});
 })
+
 
 app.post('/makemove', function(req, res) {
   // console.log('in');
